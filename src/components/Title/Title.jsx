fae54af143f125bcc8e23493a8e5e2920f7ee85d
ectionboy <125730248+ectionboy@users.noreply.github.com>
2024-04-03 22:59:36 +0300

    add Title

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React from 'react';
+import bg from '../../assets/bg.png';
+import {
+  BgContainer,
+  ContainerStyled,
+  TitleContainer,
+  TitleEl,
+} from './Title.styled';
+import { Button } from '@mui/material';
 
 const Title = () => {
   return (
-    <div>Title
-        <br />
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis quod ratione, aspernatur, ullam consequatur beatae minus minima velit possimus magnam distinctio sapiente quam libero cupiditate, fugit nisi corrupti. Dolorem illum quis quae facilis cum animi debitis optio ex fugiat neque! Eius, repellat a nam quia quaerat dolore. Velit, quasi enim accusamus amet ab officia, dolore repellendus voluptatem laboriosam magnam, deserunt repudiandae quos provident optio saepe maiores! Vero perspiciatis quos vel, dolor tempore dicta exercitationem nemo corrupti quod quibusdam expedita ipsum aperiam eos! Atque ex, molestiae vitae nisi, eum ducimus blanditiis, incidunt ea at sunt repudiandae aliquid ipsa consectetur dignissimos dicta modi hic porro. Dolor ab vero provident dolore esse iste fugit, pariatur, sit fugiat aliquam, itaque error? Culpa voluptate quis vitae ex aperiam voluptates neque aliquid veritatis consectetur excepturi nulla dolor iure provident, dolorem quae aut itaque fugit molestias natus commodi iusto facere impedit nisi cum? Dolores vel possimus quaerat rerum repellendus! Esse, cumque officiis animi ex culpa rem cum voluptatem dolorum alias. Cumque magni repudiandae incidunt itaque magnam repellat nam alias nostrum illum dicta neque at, asperiores quasi fugit. Perferendis eius obcaecati nemo necessitatibus sequi, molestias cumque iusto eum. Deleniti quae illum repellendus quis fugiat consequuntur distinctio ut id.
-    </div>
-  )
-}
+    <ContainerStyled>
+      <TitleContainer>
+        <TitleEl>Math learning platform </TitleEl>
+        <Button variant="contained" sx={{ backgroundColor: '#2eacff' }}>
+          Почнімо
+        </Button>
+      </TitleContainer>
+      <BgContainer>
+        <img src={bg} alt="bg" width={'460px'} height={'320px'} />
+      </BgContainer>
+    </ContainerStyled>
+  );
+};
 
-export default Title
+export default Title;
--- src/components/Title/Title.styled.js
+++ src/components/Title/Title.styled.js
@@ -1,28 +1,56 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
+import bg from '../../assets/bg.png';
 
-// export const ContainerStyled = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   max-width: 375px;
-//   margin: 0 auto;
-//   width: 100%;
-//   height: 'auto';
-//   padding-left: 20px;
-//   padding-right: 20px;
-//   padding-top: 40px;
-//   gap: 50px;
+export const ContainerStyled = styled.div`
+  margin: 0 auto;
+  width: 100%;
 
-//   @media screen and (min-width: 768px) {
-//     max-width: 768px;
-//     padding-left: 32px;
-//     padding-right: 32px;
-//     padding-top: '50px';
-//   }
+  background-image: url(${bg});
+  background-size: cover;
+  background-position: center;
 
-//   @media screen and (min-width: 1440px) {
-//     max-width: 1440px;
-//     padding-left: 128px;
-//     padding-right: 128px;
-//   }
-// `;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    justify-content: center;
+    background-image: none;
+  }
+
+  /*
+  @media screen and (min-width: 1440px) {
+    max-width: 1440px;
+    padding-left: 128px;
+    padding-right: 128px;
+  } */
+`;
+export const TitleContainer = styled.div`
+  background-color: #1a77d2e6;
+  padding: 68px 24px;
+  margin: 0 auto;
+  max-width: 345px;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media screen and (min-width: 768px) {
+    margin: 0;
+    background-color: inherit;
+  }
+`;
+export const BgContainer = styled.div`
+  display: none;
+  @media screen and (min-width: 768px) {
+    display: block;
+  }
+`;
+
+export const TitleEl = styled.h1`
+font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+    font-weight: 500;
+    font-size: 28px;
+    line-height: 1.2;
+text-align: center;
+color: aliceblue;
+  @media screen and (min-width: 768px) {
+color: #000;
+  }
+`;
